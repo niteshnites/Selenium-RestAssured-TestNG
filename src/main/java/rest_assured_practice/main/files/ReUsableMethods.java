293fdd911a018c3d0b0efa25081c9d6aec98a3db
Nitesh Bhardwaj <niteshnites3@gmail.com>
2024-12-26 15:59:22 +0530

    rest assured basic example

---
+++ src/main/java/rest_assured_practice/main/files/ReUsableMethods.java
@@ -0,0 +1,13 @@
+package rest_assured_practice.main.files;
+
+import io.restassured.path.json.JsonPath;
+
+public class ReUsableMethods {
+
+	
+	public static JsonPath rawToJson(String response)
+	{
+		JsonPath js1 =new JsonPath(response);
+		return js1;
+	}
+}
