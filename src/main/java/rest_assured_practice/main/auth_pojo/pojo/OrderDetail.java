7cf9caab055b8517a09360c21e31652cfc0f4f5e
Nitesh Bhardwaj <niteshnites3@gmail.com>
2025-01-12 20:03:35 +0530

    added api example ecommerce

---
+++ src/main/java/rest_assured_practice/main/auth_pojo/pojo/OrderDetail.java
@@ -0,0 +1,21 @@
+package rest_assured_practice.main.auth_pojo.pojo;
+
+public class OrderDetail {
+
+	String country;
+	String productOrderedId;
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getProductOrderedId() {
+		return productOrderedId;
+	}
+	public void setProductOrderedId(String productOrderedId) {
+		this.productOrderedId = productOrderedId;
+	}
+	
+	
+}
---
+++ src/main/java/rest_assured_practice/main/auth_pojo/pojo/Orders.java
@@ -0,0 +1,24 @@
+package rest_assured_practice.main.auth_pojo.pojo;
+
+import java.util.List;
+
+public class Orders {
+
+	private List<OrderDetail> orders;
+
+	public List<OrderDetail> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(List<OrderDetail> orders) {
+		this.orders = orders;
+	}
+	
+	
+
+
+
+	
+	
+	
+}
